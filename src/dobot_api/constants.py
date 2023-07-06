cfdaa3c5e7e67d91aa8520867d737dcc4fe7e7ae
Calastrophe <zediant@gmail.com>
2023-07-06 13:25:43 -0500

    push push push

--- src/dobot_api/constants.py
+++ src/dobot_api/constants.py
@@ -1,10 +1,14 @@
-from enum import IntEnum
+from enum import IntEnum, auto
+from strenum import StrEnum
+
 
 MOVEMENT_PORT = 30003
 REALTIME_FEEDBACK_PORT = 30004
 DASHBOARD_PORT = 29999
 
 
+# There could be more, but not documented in:
+# https://github.com/Dobot-Arm/TCP-IP-Protocol/blob/master/README-EN.md
 class DobotError(IntEnum):
     FAIL_TO_GET = -1
     COMMAND_ERROR = -10000
@@ -15,3 +19,45 @@ class DobotError(IntEnum):
     PARAMETER_RANGE_INCORRECT = -40000
     FIRST_PARAM_RANGE = -40001
     SECOND_PARAM_RANGE = -40002
+
+class RobotMode(IntEnum):
+    INIT = 1
+    BRAKE_OPEN = 2
+    RESERVED = 3
+    DISABLED = 4
+    ENABLE = 5
+    BACKDRIVE = 6
+    RUNNING = 7
+    RECORDING = 8
+    ERROR = 9
+    PAUSE = 10
+    JOG = 11
+
+class JointSelection(StrEnum):
+    J1NEG = "J1-"
+    J1POS = "J1+"
+    J2NEG = "J2-"
+    J2POS = "J2+"
+    J3NEG = "J3-"
+    J3POS = "J3+"
+    J4NEG = "J4-"
+    J4POS = "J4+"
+    J5NEG = "J5-"
+    J5POS = "J5+"
+
+
+class RobotType(IntEnum):
+    CR3 = 3
+    CR3L = 31
+    CR5 = 5
+    CR7 = 7
+    CR10 = 10
+    CR12 = 12
+    CR16 = 16
+    MG400 = 1
+    M1PRO = 2
+    NOVA2 = 101
+    NOVA5 = 103
+    CR3V2 = 113
+    CR5V2 = 115
+    CR10V2 = 120
--- src/dobot_api/dobot_api.py
+++ src/dobot_api/dobot_api.py
@@ -3,6 +3,9 @@ from typing import Optional
 from constants import *
 
 
+# TODO: The outer dashboard classes will implement all shared functionality, but the CR version will have additional methods.
+# TODO: A neater way of sharing methods across models.
+
 
 # We leave it upon the caller of the API to see if they care about the errors.
 # There should be no error handling inside of this API.
@@ -18,12 +21,36 @@ class Dobot:
 class Movement(DobotSocketConnection):
     def __init__(self, ip: str):
         super().__init__(ip, MOVEMENT_PORT)
-        
+
+    # MovJ
+    def move_joint(self, x: float, y: float, z: float, rx: float, ry: float, rz: float) -> Optional[DobotError]:
+        self.send_command("MovJ({:f}, {:f}, {:f}, {:f}, {:f}, {:f})".format(x, y, z, rx, ry, rz))
+        error_id, ret_val = self.await_reply()
+        return error_id
+    
+    # MovL
+    def move_linear(self, x: float, y: float, z: float, rx: float, ry: float, rz: float) -> Optional[DobotError]:
+        self.send_command("MovL({:f}, {:f}, {:f}, {:f}, {:f}, {:f})".format(x, y, z, rx, ry, rz))
+        error_id, ret_val = self.await_reply()
+        return error_id
+
+    # MoveJog
+    def move_jog(self, joint: JointSelection) -> Optional[DobotError]:
+        self.send_command("MovJog({})".format(joint))
+        error_id, ret_val = self.await_reply()
+        return error_id
+
+
 
 class Dashboard(DobotSocketConnection):
     def __init__(self, ip: str):
         super().__init__(ip, DASHBOARD_PORT)
-    
+
+    def turn_on(self) -> Optional[DobotError]:
+        self.send_command("PowerOn()")
+        error_id, ret_val = self.await_reply()
+        return error_id
+  
     def enable(self) -> Optional[DobotError]:
         self.send_command("EnableRobot()")
         error_id, ret_val = self.await_reply()
@@ -43,6 +70,15 @@ class Dashboard(DobotSocketConnection):
         self.send_command("ClearError()")
         error_id, ret_val = self.await_reply()
         return error_id
+
+    def robot_mode(self) -> DobotError | RobotMode:
+        self.send_command("RobotMode()")
+        error_id, ret_val = self.await_reply()
+        if error_id:
+            return error_id
+        else:
+            # TODO: Handle this if error'd
+            return RobotMode(int(ret_val))
     
  
     
