5699f867b42438917c7647aec36e87faf956c8a4
Calastrophe <zediant@gmail.com>
2023-07-11 01:41:43 -0500

    removed .format

--- src/dobot_api/util.py
+++ src/dobot_api/util.py
@@ -13,14 +13,14 @@ class DobotSocketConnection:
     def send_command(self, cmd: str) -> Tuple[Optional[DobotError], str]:
         encoded_cmd = cmd.encode("utf-8")
         self.socket.send(encoded_cmd)
-        log.debug('The command "{}" has been sent.'.format(encoded_cmd))
+        log.debug(f'The command "{encoded_cmd}" has been sent.')
         return self.await_reply()
 
     # This is a quick and easy solution, but may not cover all ErrorIDs.
     def await_reply(self) -> Tuple[Optional[DobotError], str]:
         data = self.socket.recv(1024)
         response: str = str(data, encoding="utf-8")
-        log.debug('The return message was "{}".'.format(response))
+        log.debug(f'The return message was "{response}".')
         split_response = response.split(",")
         errorID: int = int(split_response[0].strip())
         return_value: str = split_response[1].strip()
@@ -32,7 +32,7 @@ class DobotSocketConnection:
 
         
 def clamp(val: int, local_min: int, local_max: int) -> int:
-    log.info("{:d} was clamped to the range {:d}, {:d}".format(val, local_min, local_max))
+    log.info(f"{val} was clamped to the range {local_min}, {local_max}")
     return max(local_min, min(val, local_max))
 
      
