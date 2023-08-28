0f9cff3721886157bc8614eda8278dee10c8df2f
Calastrophe <zediant@gmail.com>
2023-08-28 16:13:00 -0500

    hard coded move example

--- examples/move_example.py
+++ examples/move_example.py
@@ -1,4 +1,5 @@
 from dobot_util import Dobot, DobotError
+from time import sleep
 
 if __name__ == "__main__":
     # Provide the IP of the Dobot Robot, determine if you want to log or not - read other default arguments and determine what you want to change.
@@ -10,14 +11,31 @@ if __name__ == "__main__":
     # This is a joint to joint move, so you provide the joint values you want to move to.
     # The amount of joints needed to pass depends on the robot model type.
     # For the case of an M1 Pro, it would need to be four floats.
-    result = robot.movement.joint_to_joint_move([0.0, 0.0, 0.0, 0.0])
 
-    # Any command provided through the API could potentially fail and return an error.
-    # It is important to handle ALL cases, so handle errors - either through a function which takes an error and panics, or does something based on the error.
-    if result:
-        # There is an error, handle it.
-        pass
+    # Most if not all of the API commands could potentially fail with an error which means YOU need to handle errors.
+    # It is upto the user to determine how to go about errors, ignore them, try to redo the command, or even just panic and exit.
+    # Until positioning is integrated into the move commands, you typically need to sleep between commands or check positioning yourself.
+
+    robot.movement.joint_to_joint_move([60.0, 82.0, 80.0, -35])
+    robot.dashboard.set_digital_output(1, 1)
+    sleep(3)
     
+    robot.movement.joint_to_joint_move([-10.0, 82.0, 230.00, -35])
+    sleep(1)
+    robot.movement.joint_to_joint_move([60.0, 82.0, 70.0, -35])
+    sleep(1)
+    potential_error = robot.dashboard.set_digital_output(1, 0)
+    sleep(3)
+
+    if potential_error:
+        # Handle the errors however you choose!
+        print("There was an error turning off the vacuum line.")
+
+    robot.dashboard.disable()
+
+    
+    
+
     # If you've got this far, you didn't encounter an error or handled it.
 
     # This API subscribes to the idea that the USER should handle exceptions and not force their hand by raising exceptions, but it comes with the price of simple code being verbose and needing an error handling function.
